62d9af1aecea36587dfe0c80aad4cbf89e4d6899
Joey <driscoll.recording@gmail.com>
2019-09-25 19:26:56 -0500

    first commit

---
+++ csproj_msbuild_core2/start-end/Library/src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,42 @@
+﻿using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.API.Helpers;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+        //Injecting an instance of our repository thorugh constructor injection
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+        // In this action we should get the authors from our repository and return them. But we need an instance of our repository.
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            var authors = new List<AuthorDto>();
+
+            foreach (var author in authorsFromRepo)
+            {
+                authors.Add(new AuthorDto()
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName} {author.LastName}",
+                    Genre = author.Genre,
+                    Age = author.DateOfBirth.GetCurrentAge()
+                });
+            }
+            //return the authors list variable
+            return new JsonResult(authors);
+        }
+    }
+}
---
+++ csproj_msbuild_core2/start-end/Library/src/Library.API/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Helpers
+{
+    public static class DateTimeOffsetExtensions
+    {// Helper Extension Method GetCurrentAge - Calcuate current age starting from a provided datetime offest value
+        public static int GetCurrentAge(this DateTimeOffset dateTimeOffset)
+        {
+            var currentDate = DateTime.UtcNow;
+            int age = currentDate.Year - dateTimeOffset.Year;
+
+            if (currentDate < dateTimeOffset.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
